6b599657311fc4f02a843ee7afd5a181862e903f
RavinakS <ravinasingh697@gmail.com>
2021-10-05 20:10:55 +0530

    middleware

--- README.md
+++ README.md
@@ -38,3 +38,6 @@
 ### regex
  regex to restrict URL parameter matching.
  - example - app.get('/things/:id([0-9]{5})', callback)
+
+## Middleware 
+middleware as functions that execute after the server receives the request and before the controller action sends the response. The biggest thing is that middleware functions have access to the response (res) and request (req) variables and can modify them or use them as needed. Middleware functions also have a third parameter which is a next function. This function is important since it must be called from a middleware for the next middleware to be executed. If this function is not called then none of the other middleware including the controller action will be called.
--- index.js
+++ index.js
@@ -3,20 +3,25 @@ const app = express();
 const things = require('./routes/test_things')
 
 
-app.use('/things', things);
-// app.get('/', function (req, res){
-//     res.send("Hello world!!");
-// })
+app.use(amiddleware);
 
-// app.all('/ravina', (req, res)=>{
-//     res.send("Well Come Ravina!")
-// })
+app.get('/', function (req, res){
+    res.send("Hello world!!");
+})
 
-// app.get('/:user/:id', (req, res)=>{
-//     id = req.params.id;
-//     user = req.params.user;
-//     res.send('Hey this is ' + user + " " + id)
-// })
+app.all('/ravina', (req, res)=>{
+    res.send("Well Come Ravina!")
+})
+
+app.get('/:user/:id', (req, res)=>{
+    id = req.params.id;
+    user = req.params.user;
+    res.send('Hey this is ' + user + " " + id)
+})
+
+function amiddleware(req, res, next){
+        console.log("Middleware is executed. :>)");
+}
 
 app.listen(3040, (req, res)=>{
     console.log('server is running-----');
