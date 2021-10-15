7f2ee85a347dcddf01b603f4757fa4ae171b800f
RavinakS <ravinasingh697@gmail.com>
2021-10-16 03:58:23 +0530

    created a users_posts file in the controller

---
+++ controller/userPosts.controller.js
@@ -0,0 +1,53 @@
+const userPostsTable = require('../model/user_posts');
+
+const allPosts = (req, res)=>{
+    userPostsTable.allPosts()
+    .then((response)=>{
+        res.send(response);
+    })
+    .catch((err)=>{
+        res.send(err);
+    })
+}
+
+const getPostByID = (req, res)=>{
+    userPostsTable.postByID(req.params.id)
+    .then((response)=>{
+        res.send(response);
+    })
+    .catch((err)=>{
+        res.send(err);
+    })
+}
+
+const newPost = (req, res)=>{
+    userPostsTable.newPost(req.body)
+    .then((result)=>{
+        res.send({"status-code": 200, "message": "your post is published successfully!!"})
+    })
+    .catch((err)=>{
+        res.send(err);
+    })
+}
+
+const editPost = (req, res)=>{
+    userPostsTable.editPost(req.params.id, req.body)
+    .then((response)=>{
+        res.send({"status-code": 200, "message": "Changes are saved."})
+    })
+    .catch((err)=>{
+        res.send(err)
+    })
+}
+
+const deletePost = (req, res)=>{
+    userPostsTable.deletePost(req.params.id)
+    .then((response)=>{
+        res.send({"status-code": 200, "message": "Your post is successfully deleted."})
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+}
+
+module.exports = {allPosts, getPostByID, newPost, editPost, deletePost}
--- routes/user_posts.js
+++ routes/user_posts.js
@@ -1,56 +1,15 @@
-const { response } = require('express');
 const express = require('express');
 const router = express.Router();
-const userPostsTable = require('../model/user_posts');
+const {allPosts, getPostByID, newPost, editPost, deletePost} = require('../controller/userPosts.controller');
 
-router.get('/all-posts', (req, res)=>{
-    userPostsTable.allPosts()
-    .then((response)=>{
-        res.send(response);
-    })
-    .catch((err)=>{
-        res.send(err);
-    })
-})
+router.get('/all-posts', allPosts);
 
-router.get('/post/:id', (req, res)=>{
-    userPostsTable.postByID(req.params.id)
-    .then((response)=>{
-        res.send(response);
-    })
-    .catch((err)=>{
-        res.send(err);
-    })
-})
+router.get('/post/:id', getPostByID);
 
-router.post('/new-post', (req, res)=>{
-    userPostsTable.newPost(req.body)
-    .then((result)=>{
-        res.send({"status-code": 200, "message": "your post is published successfully!!"})
-    })
-    .catch((err)=>{
-        res.send(err);
-    })
-})
+router.post('/new-post', newPost);
 
-router.put('/edit-post/:id', (req, res)=>{
-    userPostsTable.editPost(req.params.id, req.body)
-    .then((response)=>{
-        res.send({"status-code": 200, "message": "Changes are saved."})
-    })
-    .catch((err)=>{
-        res.send(err)
-    })
-})
+router.put('/edit-post/:id', editPost)
 
-router.delete('/delete-post/:id', (req, res)=>{
-    userPostsTable.deletePost(req.params.id)
-    .then((response)=>{
-        res.send({"status-code": 200, "message": "Your post is successfully deleted."})
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-})
+router.delete('/delete-post/:id', deletePost)
 
 module.exports = router;
